251497465e96d7dc8bd88d2d10f69f7e0c2832ab
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 21:26:32 -0500

    Update editar_fecha.php
    actualización de editar_fecha.php segun las recomendaciones de Sonar

--- editar_fecha.php
+++ editar_fecha.php
@@ -2,31 +2,24 @@
 
 include('conexion.php');
 
-		$id=$_POST['id'];
-		$descripcion=$_POST['descripcion'];
+        $id=$_POST['id'];
+        $descripcion=$_POST['descripcion'];
         $fechaasignacion=$_POST['fechaasignacion'];
         $fechaentrega=$_POST['fechaentrega'];	
 	
-				
-		$consulta="UPDATE tareaUsuario SET fechaentrega='$fechaentrega' where id='$id'";
-		
-		$conexion= conectar();
-		
-		$resultado=mysql_query($consulta,$conexion) or mysql_error;
+        $consulta="UPDATE tareaUsuario SET fechaentrega='$fechaentrega' where id='$id'";
+	$conexion= conectar();
+	$resultado=mysql_query($consulta,$conexion) or mysql_error;
 		
 		
-		  if($resultado){
-         	echo '<script>alert("Se realizaron los cambios")
- 			history.back()
-		 			</script>';
+       if($resultado){
+       echo '<script>alert("Se realizaron los cambios")
+              history.back()
+              </script>';
          	
-
-        
-			}else{
-         	echo '<script>alert("Fallo! No se realizaron cambios")
- 			history.back()
-		 			</script>';
-    }
-		
-		
+       }else{
+             echo '<script>alert("Fallo! No se realizaron cambios")
+             history.back()
+             </script>';
+       }
 ?>
