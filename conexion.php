9793c39c311fba758da8e6352350388ef5491840
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 20:25:50 -0500

    Update conexion.php
    actualización de conexión.php segun las recomendaciones de Sonar

--- conexion.php
+++ conexion.php
@@ -2,54 +2,55 @@
 
 function conectar(){
     	
-		$server="localhost";
-		$user="root";
-		$password="mysql";
-		$nameDataBase="acreditacion";
-  		$conexion=mysql_connect($server,$user,$password);
-        mysql_select_db($nameDataBase,$conexion);
+        $server="localhost";
+        $user="root";
+        $password="mysql";
+        $nameDataBase="acreditacion";
+        $conexion=mysql_connect($server,$user,$password);
+                  mysql_select_db($nameDataBase,$conexion);
         return $conexion;
-	}
+       }
 
 
-	function getQuery($consulta){
-		$query=mysql_query($consulta);
-		$resultado=mysql_fetch_array($query);
-		return $resultado;
-	}
+       function getQuery($consulta){
+       $query=mysql_query($consulta);
+       $resultado=mysql_fetch_array($query);
+       return $resultado;
+       }
 
 	
-	function getConsulta($consulta){
-		$query=mysql_query($consulta);
-		return $query;
-	}
-	function getInsert($consulta,$conexion){
-		if(mysql_query($consulta,$conexion)or die(mysql_error())){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+       function getConsulta($consulta){
+       $query=mysql_query($consulta);
+       return $query;
+       }
+     
+       function getInsert($consulta,$conexion){
+       if(mysql_query($consulta,$conexion)or die(mysql_error())){
+       return true;
+       }
+          else {
+       
+       return false;
+             }
+        }
 
-	function getUpdate($consulta,$conexion){
-		if(mysql_query($consulta,$conexion)or die(mysql_error())){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+      function getUpdate($consulta,$conexion){
+      if(mysql_query($consulta,$conexion)or die(mysql_error())){
+      return true;
+      }
 
-	function getDelete($consulta,$conexion){
-		if(mysql_query($consulta,$conexion)or die(mysql_error())){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+         else {
+      return false;
+           }
+      }
 
-
+     function getDelete($consulta,$conexion){
+     if(mysql_query($consulta,$conexion)or die(mysql_error())){
+     return true;
+     }
+        else {
+     return false;
+     }
+ }
 ?>
 
