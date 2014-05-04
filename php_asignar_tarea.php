dbeecb2d4f90f80cea7b25b52e426c05732a55d7
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 21:41:27 -0500

    Update php_asignar_tarea.php
    actualización de php_asignar_tarea.php segun recomendaciones de Sonar

--- php_asignar_tarea.php
+++ php_asignar_tarea.php
@@ -2,44 +2,38 @@
 
 include('conexion.php');
 
-	$id=$_POST['id'];
-	$tarea=$_POST['tarea'];
-	$propietario=$_POST['propietario'];
-	$responsable=$_POST['responsable'];
-	$fechaasignacion=$_POST['fechaasignacion'];
-	$fechaentrega=$_POST['fechaentrega'];
-	$prioridad=$_POST['prioridad'];
-	$estado=$_POST['estado'];
-	$descripcion=$_POST['descripcion'];
+        $id=$_POST['id'];
+        $tarea=$_POST['tarea'];
+        $propietario=$_POST['propietario'];
+        $responsable=$_POST['responsable'];
+        $fechaasignacion=$_POST['fechaasignacion'];
+        $fechaentrega=$_POST['fechaentrega'];
+        $prioridad=$_POST['prioridad'];
+        $estado=$_POST['estado'];
+        $descripcion=$_POST['descripcion'];
 	
 
 
-	$sql_asignar_tarea="insert into tareaUsuario (id, tarea, propietario, responsable, fechaasignacion, fechaentrega, prioridad, estado, descripcion) VALUES ('','$tarea','$propietario','$responsable','$fechaasignacion','$fechaentrega','$prioridad','$estado','$descripcion')";
+        $sql_asignar_tarea="insert into tareaUsuario (id, tarea, propietario, responsable, fechaasignacion, fechaentrega, prioridad, estado, descripcion) VALUES ('','$tarea','$propietario','$responsable','$fechaasignacion','$fechaentrega','$prioridad','$estado','$descripcion')";
 	
 
-	
 	$conexion=conectar();
-	$consulta_registro=mysql_query($sql_asignar_tarea,$conexion) or mysql_error();
+        $consulta_registro=mysql_query($sql_asignar_tarea,$conexion) or mysql_error();
 	
 
 	
-    if($consulta_registro){
-         	echo '<script>alert("Se asigno la tarea Correctamente")
- 			history.back()
-		 			</script>';
-         	
-
-        
-    }else{
+        if($consulta_registro){
+           echo '<script>alert("Se asigno la tarea Correctamente")
+           history.back()
+           </script>';
+         
+        }else{
 	
-         	echo '<script>alert("No se pudo completar la accion")
- 			history.back()
-		 			</script>';
-					
-					
-    }
+            echo '<script>alert("No se pudo completar la accion")
+            history.back()
+            </script>';
+			
+}
 	
-
-
 
 ?>
