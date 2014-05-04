ddd8f99151d5efe5d367864aa34209cbae1bb6d1
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 21:08:29 -0500

    Update consultar_tarea.php
    actualización de consultar_tarea.php segun las recomendaciones de Sonar

--- consultar_tarea.php
+++ consultar_tarea.php
@@ -3,16 +3,14 @@
 
 include('conexion.php');
 
-	$conexion=conectar();
-	
-	$id=$_SESSION[__default][user]->id;
+        $conexion=conectar();
+        $id=$_SESSION[__default][user]->id;
 	
 	
 	/*listar tareas de una persona--docente 
 	*/
-	$sql_consultar_tareas="SELECT t.`nombre`, tu.`descripcion`, tu.`prioridad`, tu.`fechaasignacion`, tu.`fechaentrega`, do.`ruta` FROM `tareaUsuario` tu JOIN `tarea` t ON tu.`tarea`=t.`id` JOIN `documento` do ON do.`tarea`=t.`id`  WHERE tu.`propietario`=$id";
-	
-	$result=mysql_query($sql_consultar_tareas,$conexion) or mysql_error();
+        $sql_consultar_tareas="SELECT t.`nombre`, tu.`descripcion`, tu.`prioridad`, tu.`fechaasignacion`, tu.`fechaentrega`, do.`ruta` FROM `tareaUsuario` tu JOIN `tarea` t ON tu.`tarea`=t.`id` JOIN `documento` do ON do.`tarea`=t.`id`  WHERE tu.`propietario`=$id";
+        $result=mysql_query($sql_consultar_tareas,$conexion) or mysql_error();
 	
 ?>	
 
@@ -20,51 +18,54 @@ include('conexion.php');
 
 <html>
 	
-	<a><b>Lista de Tareas pendientes </b></a>
-	<br /><br />
-	<table border="1" align="center">
-					<tr>
+        <a><b>Lista de Tareas pendientes </b></a>
+        <br /><br />
+        <table border="1" align="center">
+          
+              <tr>
 						
-						<td width="100"><b>Nombre</b></td>
-						<td width="100"><b>Descripcion</b></td>
-						<td width="100"><b>Prioridad</b></td>	
-						<td width="100"><b>Fecha asignacion</b></td>	
-						<td width="100"><b>Fecha entrega</b></td>
-						<td width="100"><b>Link de documento</b></td>
+                   <td width="100"><b>Nombre</b></td>
+                   <td width="100"><b>Descripcion</b></td>
+                   <td width="100"><b>Prioridad</b></td>	
+                   <td width="100"><b>Fecha asignacion</b></td>	
+                   <td width="100"><b>Fecha entrega</b></td>
+                   <td width="100"><b>Link de documento</b></td>
 
 
 						
-					</tr>
+             </tr>
+			
+             <?php while($row=mysql_fetch_array($result)){ 
+             ?>
+   
+             <tr>
+            
+            <?php
+          
+          $id=$row['id'];
+          $nombre=$row['nombre'];
+          $descripcion=$row['descripcion'];
+          $prioridad=$row['prioridad'];
+          $fechaasignacion=$row['fechaasignacion'];
+          $fechaentrega=$row['fechaentrega'];
+          $ruta=$row['ruta'];
+
+            ?>	
 				
-			<?php while($row=mysql_fetch_array($result)){ 	?>
-				<tr>
-				<?php
-					$id=$row['id'];
-					$nombre=$row['nombre'];
-					$descripcion=$row['descripcion'];
-					$prioridad=$row['prioridad'];
-					$fechaasignacion=$row['fechaasignacion'];
-					$fechaentrega=$row['fechaentrega'];
-					$ruta=$row['ruta'];
 
+                  <td><?php echo $nombre; ?></td>
+                  <td><?php echo $descripcion; ?></td>
+                  <td><?php echo $prioridad; ?></td>
+                  <td><?php echo $fechaasignacion; ?></td>
+                  <td><?php echo $fechaentrega; ?></td>
+                  <td><?php echo $ruta; ?></td>
 
-					
-				?>	
-				
-
-					<td><?php echo $nombre; ?></td>
-					<td><?php echo $descripcion; ?></td>
-					<td><?php echo $prioridad; ?></td>
-					<td><?php echo $fechaasignacion; ?></td>
-					<td><?php echo $fechaentrega; ?></td>
-					<td><?php echo $ruta; ?></td>
-
-					<td><?php echo "<a href='/mod?nombre=$nombre.&id=$id&descripcion=$descripcion&prioridad=$prioridad&ruta=$ruta'> </a>";?></td>
+                  <td><?php echo "<a href='/mod?nombre=$nombre.&id=$id&descripcion=$descripcion&prioridad=$prioridad&ruta=$ruta'> </a>";?></td>
 					
 					
-				</tr>
-				<?php }?>
+                  </tr>
+           <?php }?>
 
-	</table>
-	</html>
+         </table>
+</html>
 	
