3b6f34f42f398cec173cb570f27b68c6b7fed39b
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 20:55:41 -0500

    Update listar_tareas.php
    actualización de listar_tareas.php segun las recomendaciones de Sonar

--- listar_tareas.php
+++ listar_tareas.php
@@ -2,93 +2,97 @@
 
 include('conexion.php');
 
-	$conexion=conectar();
+       $conexion=conectar();
+      
+      
+       /*listar tareas
+        */
+        $sql_listar_tareas="Select * from tarea";
 	
-	/*listar tareas
-	*/
-	$sql_listar_tareas="Select * from tarea";
-	
-	$result=mysql_query($sql_listar_tareas,$conexion) or mysql_error();
+        $result=mysql_query($sql_listar_tareas,$conexion) or mysql_error();
 	
 	
-	/* listar lista de tareas asignadas
-	*/
-	$sql_listar_tareas_asignadas= "SELECT t.nombre, tu.`id`, tu.`fechaasignacion`, tu.`fechaentrega` FROM `tareaUsuario` tu JOIN `tarea` t ON `tarea`=t.`id`";
+       /* listar lista de tareas asignadas
+       */
+       $sql_listar_tareas_asignadas= "SELECT t.nombre, tu.`id`, tu.`fechaasignacion`, tu.`fechaentrega` FROM `tareaUsuario` tu JOIN `tarea` t ON `tarea`=t.`id`";
 	
-	$resultado= mysql_query($sql_listar_tareas_asignadas,$conexion) or mysql_error();
-	
-
+       $resultado= mysql_query($sql_listar_tareas_asignadas,$conexion) or mysql_error();
 ?>
 
 
 <html>	
 
-	<a><b>Lista de Tareas </b></a>
-	<br /><br />
-	<table border="1" align="center">
-					<tr>
+        <a><b>Lista de Tareas </b></a>
+        <br /><br />
+        <table border="1" align="center">
+
+              <tr>
 						
-						<td width="100"><b>Nombre</b></td>
-						<td width="100"><b>Descripcion</b></td>
-						<td width="100"><b>Prioridad</b></td>	
-						<td width="100"><b>Editar</b></td>	
+                <td width="100"><b>Nombre</b></td>
+                <td width="100"><b>Descripcion</b></td>
+                <td width="100"><b>Prioridad</b></td>	
+                <td width="100"><b>Editar</b></td>	
 						
-					</tr>
+              </tr>
 				
-			<?php while($row=mysql_fetch_array($result)){ 	?>
-				<tr>
-				<?php
-					$id=$row['id'];
-					$nombre=$row['nombre'];
-					$descripcion=$row['descripcion'];
-					$prioridad=$row['prioridad'];						
-				?>	
+        <?php while($row=mysql_fetch_array($result)){ 	?>
+              
+              <tr>
+       <?php
+       $id=$row['id'];
+       $nombre=$row['nombre'];
+       $descripcion=$row['descripcion'];
+       $prioridad=$row['prioridad'];						
+      
+       ?>	
 				
 
-					<td><?php echo $nombre; ?></td>
-					<td><?php echo $descripcion; ?></td>
-					<td><?php echo $prioridad; ?></td>
-					<td><?php echo "<a href='/mod?nombre=$nombre.&id=$id&descripcion=$descripcion&prioridad=$prioridad'> Editar</a>";?></td>
+              <td><?php echo $nombre; ?></td>
+              <td><?php echo $descripcion; ?></td>
+              <td><?php echo $prioridad; ?></td>  
+              <td><?php echo "<a href='/mod?nombre=$nombre.&id=$id&descripcion=$descripcion&prioridad=$prioridad'> Editar</a>";?></td>
 					
 					
-				</tr>
-				<?php }?>
+             </tr>
+       <?php }?>
 
-	</table>
+            </table>
 	
-	<br /><br />
-	<a><b>Lista de Tareas Asignadas </b></a>
-	<br /><br />
+            <br /><br />
+            <a><b>Lista de Tareas Asignadas </b></a>
+            <br /><br />
 	
-	<table border="1" align="center">
-					<tr>
+           <table border="1" align="center">
+           <tr>
 						
-						<td width="100"><b>Nombre Tarea</b></td>
-						<td width="100"><b>Fecha Asignacion</b></td>
-						<td width="100"><b>Fecha Entrega</b></td>	
-						<td width="100"><b>Editar</b></td>	
-						
-					</tr>
+              <td width="100"><b>Nombre Tarea</b></td>
+              <td width="100"><b>Fecha Asignacion</b></td>
+              <td width="100"><b>Fecha Entrega</b></td>	
+              <td width="100"><b>Editar</b></td>	
 				
-			<?php while($row=mysql_fetch_array($resultado)){ 	?>
-				<tr>
-				<?php
-					$id=$row['id'];
-					$nombretarea=$row['nombre'];
-					$fechaasignacion=$row['fechaasignacion'];
-					$fechaentrega=$row['fechaentrega'];						
-				?>	
+          </tr>
 				
-					<td><?php echo $nombretarea; ?></td>
-					<td><?php echo $fechaasignacion; ?></td>
-					<td><?php echo $fechaentrega; ?></td>
-					<td><?php echo "<a href='/mod?id=$id&nombretarea=$nombretarea&fechaasignacion=$fechaasignacion&fechaentrega=$fechaentrega'> Editar</a>";?></td>
+       <?php while($row=mysql_fetch_array($resultado)){ 	?>
+          <tr>
+       <?php
+        
+        $id=$row['id'];
+        $nombretarea=$row['nombre'];
+        $fechaasignacion=$row['fechaasignacion'];
+        $fechaentrega=$row['fechaentrega'];						
+?>	
+				
+             <td><?php echo $nombretarea; ?></td>
+             <td><?php echo $fechaasignacion; ?></td>
+             <td><?php echo $fechaentrega; ?></td>
+             <td><?php echo "<a href='/mod?id=$id&nombretarea=$nombretarea&fechaasignacion=$fechaasignacion&fechaentrega=$fechaentrega'> Editar</a>";?></td>
+ 					
 					
-					
-				</tr>
-				<?php }?>
+         </tr>
+      
+         <?php }?>
 
-	</table>
-	
+         </table>
+ 	
 
 </html>
