ad4810ba4ac5074ed709ca42a47ca58195d505c0
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 19:33:59 -0500

    Update php_modificar_tarea.php
    Actualización de php_modificar_tarea.php segun las sugerencias de Sonar

--- php_modificar_tarea.php
+++ php_modificar_tarea.php
@@ -2,20 +2,16 @@
 
 include_once('conexion.php');
 
-		$id=$_GET['id'];
+        $id=$_GET['id'];
         $nombre=$_GET['nombre'];
         $descripcion=$_GET['descripcion'];
         $prioridad=$_GET['prioridad'];	
 	
+             $conexion= getConexion();
 		
-		$conexion= getConexion();
+             $consulta="update tarea set nombre='".$nombre"', descripcion='".$descripcion"' where id='$id'";
+             $resultado=mysql_query($consulta,$conexion);
 		
-		$consulta="update tarea set nombre='".$nombre"', descripcion='".$descripcion"' where id='$id'";
-		$resultado=mysql_query($consulta,$conexion);
-		
-		echo "<script> alert('Se ejecuto la consulta'); </script>";
-		
-		
-		
-		
+             echo "<script> alert('Se ejecuto la consulta'); </script>";
+
 ?>
