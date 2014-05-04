08e94019e9f4f03e0369484765eb58d4f92fa648
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 21:37:03 -0500

    Update editar_perfil.php
    actualización de editar_perfil.php segun la recomendación de Sonar

--- editar_perfil.php
+++ editar_perfil.php
@@ -3,34 +3,32 @@
 include('conexion.php');
 
 		
-		$id=$_POST['id'];
-		$name=$_POST['name'];
+        $id=$_POST['id'];
+        $name=$_POST['name'];
         $username=$_POST['username'];
         $email=$_POST['email'];
 	
 				
-		$consulta="UPDATE acr_users SET name='$name', username='$username', email='$email' where id='$id'";
+        $consulta="UPDATE acr_users SET name='$name', username='$username', email='$email' where id='$id'";
 		
-		$conexion= conectar();
+        $conexion= conectar();
 		
 		
 		
-		$resultado=mysql_query($consulta,$conexion) or mysql_error;
+        $resultado=mysql_query($consulta,$conexion) or mysql_error;
 		
-		  if($resultado){
-         	echo '<script>alert("Se registraron los cambios")
- 			history.back()
-		 			</script>';
-         	
-			$_SESSION[__default][user]->name=$name;
-			$_SESSION[__default][user]->username=$username;
-			$_SESSION[__default][user]->email=$email;
+        if($resultado){
+        echo '<script>alert("Se registraron los cambios")
+               history.back()
+                           </script>';
+ 
+                  $_SESSION[__default][user]->name=$name;
+                  $_SESSION[__default][user]->username=$username;
+                  $_SESSION[__default][user]->email=$email;
         
-			}else{
-         	echo '<script>alert("Fallo! No se efectuaron los cambios")
- 			history.back()
-		 			</script>';
-    }
-		
-		
+         }else{
+         echo '<script>alert("Fallo! No se efectuaron los cambios")
+         history.back()
+         </script>';
+     }
 ?>
