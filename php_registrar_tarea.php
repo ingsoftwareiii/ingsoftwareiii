89036eba8eaeb2d6516a1a881dac97ac90afb152
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 20:09:20 -0500

    Update php_registrar_tarea.php
    actualización de php_registrar_tarea.php segun las recomendaciones de Sonar

--- php_registrar_tarea.php
+++ php_registrar_tarea.php
@@ -2,31 +2,25 @@
 
 include('conexion.php');
 
-	$nombre=$_POST['nombre'];
-	$descripcion=$_POST['descripcion'];
-	$prioridad=$_POST['prioridad'];
+         $nombre=$_POST['nombre'];
+         $descripcion=$_POST['descripcion'];
+         $prioridad=$_POST['prioridad'];
 
 
-	$sql_registrar_tarea="insert into tarea (id,nombre,descripcion,prioridad) 
-    VALUES('','$nombre','$descripcion','$prioridad')";
-	$conexion=conectar();
-	$consulta_registro=mysql_query($sql_registrar_tarea,$conexion) or mysql_error();
+         $sql_registrar_tarea="insert into tarea (id,nombre,descripcion,prioridad) 
+         VALUES('','$nombre','$descripcion','$prioridad')";
+         $conexion=conectar();
+         $consulta_registro=mysql_query($sql_registrar_tarea,$conexion) or mysql_error();
 
-	
-    if($consulta_registro){
-         	echo '<script>alert("Se registro la tarea Correctamente")
- 			history.back()
-		 			</script>';
-         	
-
-        
-    }else{
-         	echo '<script>alert("No se pudo registrar la tarea")
- 			history.back()
-		 			</script>';
-    }
-	
-
-
+        	if($consulta_registro){
+                echo '<script>alert("Se registro la tarea Correctamente")
+                history.back()
+                </script>';
+         	}else{
+  
+                      echo '<script>alert("No se pudo registrar la tarea")
+                      history.back()
+                      </script>';
+}
 
 ?>
