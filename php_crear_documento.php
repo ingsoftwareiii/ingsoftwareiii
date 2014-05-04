d8f0d5be263699bace55bb1e9c938b4146aa9556
ingsoftwareiii <jessica_alejandra09@hotmail.com>
2014-05-03 20:03:09 -0500

    Update php_crear_documento.php
    Actualizado php_crear_documento.php segun las recomendaciones de Sonar

--- php_crear_documento.php
+++ php_crear_documento.php
@@ -2,29 +2,22 @@
 
 include('conexion.php');
 
-	$tarea=$_POST['tarea'];
-	$nombre=$_POST['nombre'];
-	$ruta=$_POST['ruta'];
+        $tarea=$_POST['tarea'];
+        $nombre=$_POST['nombre'];
+        $ruta=$_POST['ruta'];
 
-	$sql_crear_documento="INSERT INTO `documento` (`id`, `tarea`, `nombre`, `ruta`) VALUES (NULL, '$tarea', '$nombre', '$ruta')";
-	$conexion=conectar();
-	$consulta_registro=mysql_query($sql_crear_documento,$conexion) or mysql_error();
+       $sql_crear_documento="INSERT INTO `documento` (`id`, `tarea`, `nombre`, `ruta`) VALUES (NULL, '$tarea', '$nombre', '$ruta')";
+       $conexion=conectar();
+       $consulta_registro=mysql_query($sql_crear_documento,$conexion) or mysql_error();
 
 	
-    if($consulta_registro){
-         	echo '<script>alert("Se agrego el documento Correctamente")
- 			history.back()
-		 			</script>';
-         	
-
-        
-    }else{
-         	echo '<script>alert("No se pudo agregar el documento a la tarea")
- 			history.back()
-		 			</script>';
-    }
-	
-
-
-
+              if($consulta_registro){
+                     echo '<script>alert("Se agrego el documento Correctamente")
+                     history.back()
+                     </script>';
+         	}else{
+                echo '<script>alert("No se pudo agregar el documento a la tarea")
+                history.back()
+                </script>';
+                }
 ?>
